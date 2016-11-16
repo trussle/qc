5b77eca98ebf8c54e739ee05822471edeee23b91
Jonny Arnold <jonny.arnold@trussle.com>
2016-11-16 11:56:05 +0000

    Lint JS files in trussle-qc.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,14 @@ module.exports = {
 
   env: {
     node: true
+  },
+
+  rules: {
+    // I have a dream.
+    "semi": ["error", "never"],
+
+    // We're allowed to console here;
+    // it's a set of console scripts!
+    "no-console": "off"
   }
 };
